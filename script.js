ccdc0f114494087413846b4d1e9217e5af055957
vineeth <119692597+vineeth38@users.noreply.github.com>
2023-04-29 22:41:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,37 @@
 //your JS code here. If required.
-document.getElementById("browser-info").innerHTML = navigator.appVersion;
+ const browserInfoDiv = document.getElementById('browser-info');
+    const browserName = getBrowserName();
+    const version = getBrowserVersion();
+    const message = `You are using ${browserName} version ${version}`;
+    browserInfoDiv.textContent = message;
+
+    function getBrowserName() {
+      const userAgent = navigator.userAgent;
+      if (userAgent.indexOf("Firefox") !== -1) {
+        return "Firefox";
+      } else if (userAgent.indexOf("Chrome") !== -1) {
+        return "Chrome";
+      } else if (userAgent.indexOf("Edge") !== -1) {
+        return "Microsoft Edge";
+      } else if (userAgent.indexOf("Safari") !== -1) {
+        return "Safari";
+      } else {
+        return "Unknown browser";
+      }
+    }
+ function getBrowserVersion() {
+      const userAgent = navigator.userAgent;
+      let version;
+      if (userAgent.indexOf("Firefox") !== -1) {
+        version = userAgent.match(/Firefox\/(\d+)/)[1];
+      } else if (userAgent.indexOf("Chrome") !== -1) {
+        version = userAgent.match(/Chrome\/(\d+)/)[1];
+      } else if (userAgent.indexOf("Edge") !== -1) {
+        version = userAgent.match(/Edge\/(\d+)/)[1];
+      } else if (userAgent.indexOf("Safari") !== -1) {
+        version = userAgent.match(/Version\/(\d+)/)[1];
+      } else {
+        version = "Unknown version";
+      }
+      return version;
+    }
